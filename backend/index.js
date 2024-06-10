dd1a506e18c19674d62f823ba8b27976b4530673
JagadheeshM <maheshwaramjagadheesh@gmail.com>
2024-06-11 00:41:48 +0530

    Added get-all-notes endpoint

--- backend/index.js
+++ backend/index.js
@@ -167,7 +167,26 @@ app.put("/edit-note/:noteId",authenticateToken,async(req,res)=>{
             message:"Internal Server Error",
         });
     }
-})
+});
+
+app.get("/get-all-notes/",authenticateToken,async(req,res)=>{
+    const {user} = req.user;
+    
+    try{
+        const notes = await Note.find({userId:user._id}).sort({isPinned:-1});
+        return res.json({
+            error:false,
+            notes,
+            message:"All notes retrived successfully",
+        });
+    }
+    catch(error){
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error",
+        });
+    }
+});
 
 app.listen(8000);
 
