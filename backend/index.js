c67aac42b40e86adc3420f5040c4f95a60e6c03e
JagadheeshM <maheshwaramjagadheesh@gmail.com>
2024-06-12 17:48:47 +0530

    added get-user api

--- backend/index.js
+++ backend/index.js
@@ -95,6 +95,21 @@ app.get("/login",async(req,res)=>{
     }
 });
 
+app.get("/get-user",authenticateToken ,async(req,res)=>{
+    const {user} = req.user;
+
+    const isUser = await User.findOne({_id:user._id});
+
+    if(!isUser){
+        return res.sendStatus(401);
+    }
+
+    return res.json({
+        user:{fullName:isUser.fullName,email:isUser.email,"_id":isUser._id,createdOn:isUser.createdOn},
+        message:"",
+    });
+});
+
 app.post("/add-note",authenticateToken,async(req,res)=>{
     const {title,content,tags} = req.body;
     const {user} = req.user;
