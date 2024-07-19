1c227cb6f3e99df1442de245005b38ddedadcafd
JagadheeshM <maheshwaramjagadheesh@gmail.com>
2024-07-19 11:43:45 +0530

    final1

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import Welcom from "./pages/Welcome/Welsom";
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Welcome />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/Signup" exact element={<SignUp />} />
